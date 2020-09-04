60ed2369634e34459684d2f9c403c301162db0f6
Suraj Patil <srpatil@uci.edu>
2020-09-04 23:23:11 +0530

    Define memq operation

--- quotation.scm
+++ quotation.scm
@@ -6,3 +6,17 @@
 
 (car '(a b c)) ;a 
 (cdr '(a b c)) ;(b c)
+
+(define (memq item x)
+	(cond ((null? x) false) 
+		((eq? item (car x)) x) 
+		(else (memq item (cdr x)))))
+
+(memq 'apple '(pear banana prune)) ;false
+(memq 'pear '(pear banana prune)) ;true
+(list (list 'george))	;((george))
+(cdr '((x1 x2) (y1 y2))) ; ((y1 y2))
+
+; cadr takes car of a cdr
+(cadr '((x1 x2) (y1 y2))) ; (y1 y2)
+
