3eb50ac3f134817d07723a4d6394e83ec2def751
Suraj Patil <srpatil@uci.edu>
2020-09-05 22:00:27 +0530

    Edit deriv procedure

--- differentiation.scm
+++ differentiation.scm
@@ -52,5 +52,31 @@
 (deriv '(+ x 3) 'x) ; (+ 1 0)
 (deriv '(* x y) 'x) ; (+ (* x 0) (* 1 y))
 
+; We change make-sum procedure to that if both summands are numbers, make-sum will return their sum.
+
+(define (make-sum a1 a2) 
+    (cond ((=number? a1 0) a2) 
+          ((=number? a2 0) a1) 
+          ((and (number? a1) (number? a2)) 
+            (+ a1 a2)) 
+          (else (list '+ a1 a2))))
+
+; Similarly, we change make-product 0 times anything is 0 and 1 times anything is the number itself
+
+; This uses the procedure =number?, which checks whether an expression is equal to a given number:
+
+(define (=number? exp num) 
+        (and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0) 
+          ((=number? m1 1) m2) 
+          ((=number? m2 1) m1) 
+          ((and (number? m1) (number? m2)) (* m1 m2)) 
+          (else (list '* m1 m2))))
+
+(deriv '(+ x 3) 'x) ; 1
+(deriv '(* x y) 'x) ; y 
+
 
 
